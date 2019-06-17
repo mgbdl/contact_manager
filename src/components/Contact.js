b5c4601c4b570fbdaad0d1cd46db8f64df94a234
Miguel Breceda <miguel.breceda@outlook.es>
2019-06-17 12:39:19 -0500

    Simple Contact Manager Update: *Add Contact component which storage a single Contact *Add Contacts which has the hard-coded contact Object (JSON) *Add Awesome Font and Bootrstrap *Delete some boileraplate styles

---
+++ src/components/Contact.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onDeleteClick = id => {
+    this.props.deleteClickHandler(id);
+  };
+
+  render() {
+    const { id, name, phone, email } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name}{' '}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+            style={{ cursor: 'pointer' }}
+          />
+          <i
+            className="fas fa-times"
+            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+            onClick={this.onDeleteClick.bind(this, id)}
+          />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">{email}</li>
+            <li className="list-group-item">{phone}</li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
+};
+
+export default Contact;
