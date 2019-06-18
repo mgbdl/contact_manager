3eafcebf523d46ffe3f224f93de94001141f2573
Miguel Breceda <miguel.breceda@outlook.es>
2019-06-18 13:50:25 -0500

    Updates

---
+++ precache-manifest.ff8e1afdfabc0c60d63a7a8658cbca40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd2b82b183f072ab28266acae5ec7d7",
+    "url": "/github.io/contact_manager/index.html"
+  },
+  {
+    "revision": "e9fd23a0cffe0c30b95a",
+    "url": "/github.io/contact_manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "454f9bf9c91ae48acb7c",
+    "url": "/github.io/contact_manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e9fd23a0cffe0c30b95a",
+    "url": "/github.io/contact_manager/static/js/2.9eec12bf.chunk.js"
+  },
+  {
+    "revision": "454f9bf9c91ae48acb7c",
+    "url": "/github.io/contact_manager/static/js/main.ef3640d1.chunk.js"
+  },
+  {
+    "revision": "7f382cf702413e0d593c",
+    "url": "/github.io/contact_manager/static/js/runtime~main.06796c2e.js"
+  }
+]);
