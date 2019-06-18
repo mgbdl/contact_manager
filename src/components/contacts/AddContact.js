ccfc3e42e7e091a21d8b017200045dd84230b4df
Miguel Breceda <miguel.breceda@outlook.es>
2019-06-18 09:29:03 -0500

    Add Contact Functiionality Update: *Add new action dispatcher ADD_CONTACT *Install uuid to generate ids *Create add contact component with refs by defatul *Add action ADD_CONTACT to AddContact Component *Crate TextInputComponent and apply to AddContact

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import { Consumer } from '../../context';
+import uuid from 'uuid';
+
+import TextInputGroup from '../layout/TextInputGroup';
+
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+
+  // Necesary to make the form works
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = (dispatch, e) => {
+    e.preventDefault();
+
+    const { name, email, phone } = this.state;
+    const newContact = { id: uuid(), name, email, phone };
+
+    dispatch({ type: 'ADD_CONTACT', payload: newContact });
+
+    this.setState({
+      name: '',
+      email: '',
+      phone: ''
+    });
+  };
+
+  render() {
+    const { name, email, phone } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+
+          return (
+            <div className="card mb-3">
+              <div className="card-header">Add Contact</div>
+              <div className="card-body">
+                <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter Name..."
+                    value={name}
+                    onChange={this.onChange}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    type="email"
+                    name="email"
+                    placeholder="Enter Email..."
+                    value={email}
+                    onChange={this.onChange}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter Phone..."
+                    value={phone}
+                    onChange={this.onChange}
+                  />
+                  <input
+                    type="submit"
+                    value="Add Contact"
+                    className="btn btn-light btn-block"
+                  />
+                </form>
+              </div>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default AddContact;
